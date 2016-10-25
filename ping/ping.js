82d2b96b3933e28bafe1e9683d563082a22c9ec6
fun <ayoahy@gmail.com>
2016-10-25 16:51:03 +0800

    refs 1.去除pingError脚本，单个脚本执行多次ping的操作 2.添加命令行校验，需要增加--num参数，来进行多次ping操作

--- ping/ping.js
+++ ping/ping.js
@@ -1,40 +1,13 @@
-var fs         = require("fs");
-var ping       = require('../index');
-var path       = require('path')
+var ping = require('../lib/f-ping'),
+    num  = require('../lib/cli-commander')();
+console.log(num);
+var i = 1;
+var pingHandler = ping(true);
+while (i < num) {
 
-var rootPath   = path.resolve(__dirname, '..')
-var inputPath  = rootPath + '/data/sites.csv';
-var outputPath = rootPath + '/data/output.csv';
+    pingHandler = pingHandler.then(function(ret) {
+        return ping(false);
+    });
 
-//每次都会删除原有的output文件
-fs.unlink(outputPath, function(err) {});
-
-var hostsText = fs.readFileSync(inputPath, "utf-8");
-var hosts = hostsText.split("\n");
-
-console.log("===========================begin ping=================================")
-
-hosts.forEach(function (host, index) {
-    var outpu_prefix = "共:" + hosts.length + "--当前:" + (index+1) + "--域名:" + host + "--结果:"; 
-    host = trim(host);
-    var ret = "";
-    ping.promise.probe(host, {
-        timeout: 2,
-        extra: ["-i 2"],
-    })
-    .then(function (res) {
-        if(res.time) {
-            ret = res.output.split("\n")[0].split(" ")[2];
-        } else {
-            ret = "失败！！";
-        }       
-        console.log(outpu_prefix + ret);
-        fs.appendFile(outputPath, host + ',' + ret + "\n", function(error) {});
-    })
-    .done();
-});
-
-
-function trim(str){ //删除左右两端的空格
-　　     return str.replace(/(^s*)|(s*$)/g, "");
-　　 }
+    i++;
+}
